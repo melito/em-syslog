a461d0ebb2064fc9db7ee7f0f09db384c8de7f00
Mel Gray <melgray@gmail.com>
2009-06-30 14:59:08 -0700

    Dusting everything off a bit.  Still not 100% happy with this, but it works with syslog-ng on my vmware instance.

--- test.rb
+++ test.rb
@@ -1,22 +1,31 @@
 require "lib/em/syslog"
 
-SYSLOG_SERVER = '0.0.0.0'
-SYSLOG_PORT   = 123456
+SYSLOG_SERVER = '192.168.249.134'
+SYSLOG_PORT   = 514
 
-# EM.kqueue
-# EM.epoll
+#EM.kqueue
+#EM.epoll
 EM.run {
   
   # This is gonna be our 'syslog' server for testing to make sure we're getting the correct packets
-  EM.open_datagram_socket SYSLOG_SERVER, SYSLOG_PORT do |c|
-    def c.receive_data data
-      p data      
-    end
-  end
+  #EM.open_datagram_socket SYSLOG_SERVER, SYSLOG_PORT do |c|
+  #  def c.receive_data data
+  #    p data      
+  #  end
+  #end
   
-  @logger = EM.syslog_server(SYSLOG_SERVER, SYSLOG_PORT)
-  EM.add_periodic_timer(1) {
-    @logger.alert("#{Time.now.to_s}")
-  }
+  EM.add_periodic_timer(10) {    
+    EM.syslog_setup(SYSLOG_SERVER, SYSLOG_PORT)
+    
+    EM.emergency('emergency')
+    EM.alert('alert')
+    EM.critical('critical')
+    EM.error('error')
+    EM.warning('warning')
+    EM.notice('notice')
+    EM.informational('informational')
+    EM.info('info')
+    EM.debug('debug')
+  } 
   
 }
